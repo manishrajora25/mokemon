bf38d161da0fa018d7ca31a1b38fe4309332d655
manishrajora25 <manishrajora453@gmail.com>
2025-02-05 17:42:07 +0530

    pokemon

--- script.js
+++ script.js
@@ -1,53 +1,83 @@
-import { PokemonData } from "./data.js";
 
-const PokemonContainer = document.querySelector(".hellomain");
-const SelectBox = document.querySelector("#SelectBox");
-async function FetchingPokemon(url) {
-  
-    const response = await fetch(url);
-    const data = await response.json();
-    
+const nameData = document.querySelector("#hellotask");
+
+let url = "https://pokeapi.co/api/v2/pokemon?limit=40&offset=18";
+
+
+window.addEventListener("load", async () => {
+    const pokemonData = await loadPokemon(url);
+    console.log(pokemonData);
+    appendImage(pokemonData);
+});
+
+async function loadPokemon(pokiData) {
+   
+        const response = await fetch(pokiData);
+        const result = await response.json();
+
+        const promised = result.results.map(async (object) => {
+            const pokemonData = await fetch(object.url);
+            return pokemonData.json();
+        });
+
+        return await Promise.all(promised);
+    } 
+   
+
+function appendImage(data) {
+    for (let i = 0; i < data.length; i++) {
+        const parent = document.createElement("div");
+        parent.classList = "box";
+
+        const parent2 = document.createElement("div2");
+        parent2.classList.add = "box1";
+
+        const image = document.createElement("img");
+        image.src = data[i].sprites.other.dream_world.front_default; 
+
+        const heading = document.createElement("h3");
+        heading.innerText = data[i].name;
+
+        const para = document.createElement("p");
+        para.innerText = data[i].types[0].type.name;
+
+
+        const selectElement = document.createElement("select"); 
+        selectElement.id = "pokemonSelect";
       
-    console.log(error);
-  }
+
+        parent.append(image);
+        parent.append(heading);
+        parent.append(para);
+        nameData.append(parent);
 
 
-async function PokemonDisplay() {
-  for (const user of PokemonData) {
-    const { image, types } = await FetchingPokemon(user.url);
-    
-    const div = document.createElement("div");
-    div.classList.add("PokemonData");
 
-    const img = document.createElement("img");
-    img.classList.add("PokemonImage");
-    img.src = image;
-
-    const h1 = document.createElement("h1");
-    h1.classList.add("PokemonName");
-    h1.innerText = user.name;
-
-    const typeText = document.createElement("p");
-    typeText.classList.add("PokemonType");
-    typeText.innerText = `${types.join(", ")}`;
-
-    div.append(img, h1, typeText);
-    PokemonContainer.append(div);
-
-    allPokemon.push(div); 
-  }
+        
+    }
 }
 
-async function fetchingTypes() {
 
-    const response = await fetch("https://pokeapi.co/api/v2/type/?limit=21");
-    const data = await response.json();
-    
-    data.results.forEach((type) =>{
-      const option = document.createElement("option");
-      option.value = type.name;
-      option.innerText = type.name;
-      // console.log(option)
-      SelectBox.appendChild(option);
-    });
-  }
+
+
+
+
+
+
+
+
+
+        
+
+
+
+
+
+
+
+
+
+
+
+
+
